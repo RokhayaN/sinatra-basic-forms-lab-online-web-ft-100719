b01eb4ece6f809ea4790f78376c46c3e7b97f910
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-12-01 01:41:35 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,6 +2,9 @@ class Puppy
   attr_accessor :name, :breed, :age
   @@all=[]
   def initialize()
+    @name = name
+    @breed = breed
+    @age = age
    puppy= self.new
    @@all << self 
    def self.all
